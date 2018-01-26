be6c391d4835a9956387e27d35036b3ef7d5e085
John Crisostomo <johncrisostomo@protonmail.ch>
2018-01-26 12:09:13 +0800

    Removed RESTify with Node core

--- index.js
+++ index.js
@@ -1,56 +1,57 @@
-'use strict'
+'use strict';
 
-var etag = require('./etag')
-var pkg = JSON.stringify(require('./package.json'))
+var etag = require('./etag');
+var pkg = JSON.stringify(require('./package.json'));
 
-var restify = require('restify')
-var server = restify.createServer()
-var count = 1
+// var restify = require('restify');
+const http = require('http');
+// var server = restify.createServer()
 
-server.get('/a', function (req, res, next) {
-  var tag = etag(pkg + ++count)
+const server = http.createServer((req, res) => {
+  if (req.url === '/a') {
+    var tag = etag(pkg + ++count);
 
-  if (!(tag instanceof Error)) {
-    res.setHeader('ETag', tag)
+    if (!(tag instanceof Error)) {
+      res.setHeader('ETag', tag);
+    }
+
+    res.end(pkg);
   }
 
-  res.send(pkg)
-  return next()
-})
+  if (req.url === '/b') {
+    var tag = etag({ entity: pkg + ++count, algorithm: 'sha256' });
 
-server.get('/b', function (req, res, next) {
-  var tag = etag({entity: pkg + ++count, algorithm: 'sha256'})
+    if (!(tag instanceof Error)) {
+      res.setHeader('ETag', tag);
+    }
 
-  if (!(tag instanceof Error)) {
-    res.setHeader('ETag', tag)
+    res.end(pkg);
   }
 
-  res.send(pkg)
-  return next()
-})
+  if (req.url === '/c') {
+    var tag = etag(pkg + ++count, { algorithm: 'sha512WithRsaEncryption' });
 
-server.get('/c', function (req, res, next) {
-  var tag = etag(pkg + ++count, {algorithm: 'sha512WithRsaEncryption'})
+    if (!(tag instanceof Error)) {
+      res.setHeader('ETag', tag);
+    }
 
-  if (!(tag instanceof Error)) {
-    res.setHeader('ETag', tag)
+    res.end(pkg);
   }
+});
 
-  res.send(pkg)
-  return next()
-})
+var count = 1;
 
-server.listen(3000)
+server.listen(3000);
 
-var signal = 'SIGINT'
+var signal = 'SIGINT';
 
 // Cleanly shut down process on SIGINT to ensure that perf-<pid>.map gets flushed
-process.on(signal, onSignal)
+process.on(signal, onSignal);
 
-function onSignal () {
-  console.error('count', count)
+function onSignal() {
+  console.error('count', count);
   // IMPORTANT to log on stderr, to not clutter stdout which is purely for data, i.e. dtrace stacks
-  console.error('Caught', signal, ', shutting down.')
-  server.close()
-  process.exit(0)
+  console.error('Caught', signal, ', shutting down.');
+  server.close();
+  process.exit(0);
 }
