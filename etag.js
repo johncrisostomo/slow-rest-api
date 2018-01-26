419af9e744db7f69b394977bea7d22460e5aad95
John Crisostomo <johncrisostomo@protonmail.ch>
2018-01-26 11:07:15 +0800

    First optimization done

--- etag.js
+++ etag.js
@@ -1,70 +1,72 @@
-'use strict'
+'use strict';
 
-var crypto = require('crypto')
+var crypto = require('crypto');
 
-var etagCache = require('./etag-cache.json')
+var etagCache = require('./etag-cache.json');
 
-module.exports = etag
+module.exports = etag;
 
-function etag (entity, opts) {
+function etag(entity, opts) {
   if (Object(entity) === entity) {
-    opts = entity
-    entity = opts.entity
+    opts = entity;
+    entity = opts.entity;
   }
 
-  var error = false
-  opts = opts || {}
-  opts.algorithm = opts.algorithm || 'md5'
-  opts.encoding = opts.encoding || 'utf8'
-  opts.output = opts.output || 'base64'
+  var error = false;
+  opts = opts || {};
+  opts.algorithm = opts.algorithm || 'md5';
+  opts.encoding = opts.encoding || 'utf8';
+  opts.output = opts.output || 'base64';
 
-  var match
+  // Removing the code block below because caching is a premature optimization
+  // in this case
 
-  Object.keys(etagCache).forEach(function (k) {
-    match = (etagCache[k].algorithm === opts.algorithm &&
-      etagCache[k].encoding === opts.encoding &&
-      etagCache[k].output === opts.output &&
-      entity === Buffer.from(etagCache[k].content.data).toString()) &&
-      k
-  })
+  // var match;
 
-  if (match) { return match }
+  // Object.keys(etagCache).forEach(function(k) {
+  //   match =
+  //     etagCache[k].algorithm === opts.algorithm &&
+  //     etagCache[k].encoding === opts.encoding &&
+  //     etagCache[k].output === opts.output &&
+  //     entity === Buffer.from(etagCache[k].content.data).toString() &&
+  //     k;
+  // });
 
-  var hash
+  // if (match) {
+  //   return match;
+  // }
+
+  var hash;
 
   try {
-    hash = crypto
-      .createHash(opts.algorithm)
-      .update(entity, opts.encoding)
+    hash = crypto.createHash(opts.algorithm).update(entity, opts.encoding);
   } catch (e) {
-    error = true
+    error = true;
   }
 
   if (!opts.output || opts.output === 'base64') {
     try {
-      hash = hash
-        .digest('base64')
-        .replace(/=+$/, '')
+      hash = hash.digest('base64').replace(/=+$/, '');
     } catch (e) {
-      error = true
+      error = true;
     }
 
     if (!error) {
-      return hash
+      return hash;
     }
   }
 
   try {
-    hash = hash.digest(opts.output)
+    hash = hash.digest(opts.output);
   } catch (e) {
-    error = true
+    error = true;
   }
 
   if (error) {
-    return Error('oh oh')
+    return Error('oh oh');
   }
 
-  return hash
+  return hash;
 
   /**
    *  DUMAIN. Will you vouchsafe with me to change a word?
